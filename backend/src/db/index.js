a162925cfce7f68c6db05186a6147e8d22cd4c56
serendipityMist <serendipitymist@gmail.com>
2026-07-12 17:55:35 +0545

    Database connection code add

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,1 +1,16 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`);
+        console.log(`MongoDB Connected Successfully ${connectionInstance.connection.name}`);
+        
+    } catch (error) {
+        console.log(`MongoDB Connection Failed ${error}`);
+        process.exit(1);
+        
+    }
+}
+
+export {connectDB};
