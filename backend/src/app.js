9925a72afeeb50d0b6fd918c3f9e21890cfb0315
serendipityMist <serendipitymist@gmail.com>
2026-07-12 18:24:39 +0545

    prettier config file add

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookieparser";
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
