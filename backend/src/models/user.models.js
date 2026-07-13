fabc406370942f871ded06419ae9d8b33ce1b0f4
serendipityMist <serendipitymist@gmail.com>
2026-07-13 20:56:13 +0545

    password hashing code fixed

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -64,8 +64,13 @@ const userSchema = new mongoose.Schema({
 
 
 //need to check this part if it is working or i am missing something
-userSchema.pre("save",function (){
-    bcrypt.hash(this.password,10);
+//fixed it
+userSchema.pre("save", async function (next){
+
+    if(!this.isModified("password")) return next();
+
+    this.password=bcrypt.hash(this.password,10);
+    next();
 })
 
 export const User = mongoose.model("User",userSchema);
