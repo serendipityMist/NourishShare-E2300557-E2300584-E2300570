6cf063c5dc0b7e38105404e8d108bfa65a17fab7
serendipityMist <serendipitymist@gmail.com>
2026-07-18 19:35:30 +0545

    logout route and verifyJWT middleware added in the route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -16,6 +17,7 @@ router.route("/register").post(
 
 //login
 router.route("/login").post(loginUser);
-    
+//secure routes
+router.route("/logout").post(verifyJWT,logoutUser)    
 
 export default router;
