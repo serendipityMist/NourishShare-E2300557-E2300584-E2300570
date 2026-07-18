6cfecbfdbc981ab85b67bec95c0a9adc9a1ad0d6
serendipityMist <serendipitymist@gmail.com>
2026-07-18 20:22:15 +0545

    refreshToken route set

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -18,6 +18,7 @@ router.route("/register").post(
 //login
 router.route("/login").post(loginUser);
 //secure routes
-router.route("/logout").post(verifyJWT,logoutUser)    
+router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/refreshToken").post(refreshAccessToken);    
 
 export default router;
