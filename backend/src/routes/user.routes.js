93f54363263fe84e2218d30dd1133c38cf6b7cc7
serendipityMist <serendipitymist@gmail.com>
2026-07-18 22:33:20 +0545

    reset password route setup

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser, resetPassword } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -19,6 +19,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 //secure routes
 router.route("/logout").post(verifyJWT,logoutUser);
-router.route("/refreshToken").post(refreshAccessToken);    
+router.route("/refreshToken").post(refreshAccessToken);  
+router.route("/resetPassword").post(resetPassword);  
 
 export default router;
