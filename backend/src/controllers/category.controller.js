53391b81b2ad35436b473a4ea78ded1c7fa6f0d0
serendipityMist <serendipitymist@gmail.com>
2026-07-22 10:22:42 +0545

    contrller updated

--- backend/src/controllers/category.controller.js
+++ backend/src/controllers/category.controller.js
@@ -1,19 +1,23 @@
 import { Category } from "../models/category.model.js";
-import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiReponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { DEFAULT_CATEGORY_NAMES } from "../utils/defaultCategories.js";
+
+async function ensureDefaultCategories() {
+    const existingCount = await Category.countDocuments();
+    if (existingCount > 0) {
+        return Category.find().sort({ name: 1 });
+    }
+
+    await Category.insertMany(
+        DEFAULT_CATEGORY_NAMES.map((name) => ({ name }))
+    );
+
+    return Category.find().sort({ name: 1 });
+}
 
 const getCategories = asyncHandler(async (req, res) => {
-
-    const categories = await Category.find();
-
-    console.log("Database:", Category.db.name);
-    console.log("Collection:", Category.collection.name);
-    console.log("Categories:", categories);
-
-    if (categories.length === 0) {
-        throw new ApiError(404, "No categories found");
-    }
+    const categories = await ensureDefaultCategories();
 
     return res
         .status(200)
